2f811d3f915daf99f0cc3264f49ed284c10f62a3
Ganji Rupa Vani <rupaganji48@gmail.com>
2026-03-16 14:33:20 +0530

    Fix ChromeDriver for Jenkins Windows

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -2,12 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 def get_driver():
+
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
-    options.add_argument("--disable-notifications")
+
+    # Required for Jenkins
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     service = Service(ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service, options=options)
+
     return driver
