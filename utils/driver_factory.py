9ade1fc5119f353ca6072772870667bf4d7a5fd6
Ganji Rupa Vani <rupaganji48@gmail.com>
2026-03-16 14:39:25 +0530

    Fix ChromeDriver issue

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def get_driver():
 
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
 
-    # Required for Jenkins
+    # Jenkins compatible options
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
